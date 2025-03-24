fe829cef200295a5d84291b01810360522345f7a
levrone24 <levfrmdamntns@gmail.com>
2025-03-24 21:02:27 +0000

    updated scripts

--- hypr/scripts/changeWallpaper.sh
+++ hypr/scripts/changeWallpaper.sh
@@ -6,6 +6,9 @@ PICS=($(ls "${DIR}"))
 # Select a random picture from the array
 RANDOM_PICS=${PICS[$((RANDOM % ${#PICS[@]}))]}
 
+# Save the selected wallpaper to a file
+#echo "${DIR}/${RANDOM_PICS}" > ~/.cache/hellwal/last_wallpaper
+
 # Check if swaybg is running and kill it if necessary
 if [[ $(pidof swaybg) ]]; then 
     pkill swaybg
@@ -16,3 +19,12 @@ swww query || swww init
 
 # Set the selected wallpaper with transition effects
 swww img "${DIR}/${RANDOM_PICS}" --transition-fps 30 --transition-type outer --transition-duration 1.5
+
+
+ #Apply Hellwal colors
+hellwal -i "${DIR}/${RANDOM_PICS}" &
+
+# Reload colors in terminals
+wal -R
+
+
